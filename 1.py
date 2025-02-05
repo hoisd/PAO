ef336ad03c311d1de6eb0c9832a0da3109f3ad4d
hoisd <b.e.d0@mail.ru>
2025-02-05 15:12:04 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,12 @@
+P = "february"
+print(P[0])
+print(P[-1])
+print(P[2])
+print(P[-3])
+print(len(P))
+S = "We couldn't verify your mother's maiden name"
+print(S[:8])
+St= int((len(S))/2) - 2
+En= int((len(S))/2) + 2
+print(S[St:En])
+print(S[::3])
