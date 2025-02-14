ae4f6419daa948fd4dcdde2f34fac8d67dd8d8b0
hoisd <b.e.d0@mail.ru>
2025-02-14 16:35:09 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,11 @@
+spisok = ['Kanistra','Lodka', 'Motor', 'Tranec', 'Kresla', 'podramnik', 'gripsi']
+print(spisok)
+
+spisok[3:5] = ['Diski sceplenia','Knopki']
+print(spisok)
+
+spisok+= ['Spici', 'Lapka KPP']
+print(spisok)
+
+spisok[0] = []
+print(spisok)
---
+++ 3.py
@@ -0,0 +1,16 @@
+slovar = {'Beta':9, 'KTM':15, 'Husqvarna':322, 'GasGas':1, 'Sherco':52}
+print(slovar)
+
+print(slovar['Sherco'])
+
+slovar['Husqvarna'] = 12
+slovar['Beta'] = 21
+slovar['Sherco'] = 13
+print(slovar)
+
+slovar['Rieju'] = 76
+slovar['Husaberg'] = 2
+print(slovar)
+
+slovar['Beta'] = []
+print(slovar)
