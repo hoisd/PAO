0372a7c99f927b171e60dfdfbf45e4045c1bd19c
hoisd <b.e.d0@mail.ru>
2025-02-21 17:05:48 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,9 @@
+Zip = {'Bolt_Eng_M8':9, 'Bolt_Eng_M6':15, 'Shaib_M6':322, 'Bolt_Eng_M10':1, 'BuHT_plast_M6':52}
+print(Zip)
+
+New_zip = 'Bolt_Eng_M8'
+
+if New_Zip == 'Bolt_Eng_M8' and slovar['Bolt_Eng_M8'] < 18 # Необходимый запас болтов крепления головы - 18 шт.
+    print(str(18 - Zip['Bolt_Eng_M8']))
+    print('Необходимо заказать позицию "Bolt_Eng_M8" в количестве: ' + zakaz)
+
