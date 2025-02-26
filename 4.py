2836fe1939ccb01f740aefd0b7b2014a33c5c700
hoisd <b.e.d0@mail.ru>
2025-02-26 17:35:43 +0300

    Add files via upload

--- 4.py
+++ 4.py
@@ -1,9 +1,26 @@
-Zip = {'Bolt_Eng_M8':9, 'Bolt_Eng_M6':15, 'Shaib_M6':322, 'Bolt_Eng_M10':1, 'BuHT_plast_M6':52}
+Zip = {'Bolt_Eng_M8':202, 'Bolt_Eng_M6':460, 'Shaiba_M6':98, 'Bolt_Eng_M10':46, 'BuHT_plast_M6':526}
 print(Zip)
 
-New_zip = 'Bolt_Eng_M8'
+New_Zip = 'Bolt_Eng_M8'
+if New_Zip == 'Bolt_Eng_M8' and Zip['Bolt_Eng_M8'] < 310: # Необходимый запас болтов крепления головы - 18 шт.
+    print('Необходимо дозаказать позицию "Bolt_Eng_M8"')
 
-if New_Zip == 'Bolt_Eng_M8' and slovar['Bolt_Eng_M8'] < 18 # Необходимый запас болтов крепления головы - 18 шт.
-    print(str(18 - Zip['Bolt_Eng_M8']))
-    print('Необходимо заказать позицию "Bolt_Eng_M8" в количестве: ' + zakaz)
+New_Zip = 'Bolt_Eng_M10'
+if New_Zip == 'Bolt_Eng_M10' and Zip['Bolt_Eng_M10'] < 112: # Необходимый запас болтов крепления двигателя - 4 шт.
+        zakaz = str(112 - Zip['Bolt_Eng_M10'])
+        print('Необходимо заказать позицию "Bolt_Eng_M10" в количестве: ' + zakaz)
+else:
+        print('Позиции "Bolt_Eng_M10" - достаточно')
 
+New_Zip = 'Shaiba_M6'
+if New_Zip == 'Shaiba_M6' and Zip['Shaiba_M6'] < 50: # Необходимый запас шайб на рабочем месте - 50 шт.
+        zakaz = str(50 - Zip['Shaiba_M6'])
+        print('Необходимо заказать позицию "Shaiba_M6" в количестве: ' + zakaz)
+else:
+    if New_Zip == 'Shaiba_M6' and Zip['Shaiba_M6'] > 80: # В коробоку с шайбами вмещаеся 100 шт.
+        izb = str(Zip['Shaiba_M6'] - 80)
+        print('Необходимо переложить избыток - ' + izb + ' на склад')
+        Zip['Shaiba_M6'] = 80
+    else:
+        print('Позиции "Shaiba_M6" - достаточно')
+print(Zip)
